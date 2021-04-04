bb77d5704fa0cd25914b19083cbc8bc68f41e292
chill-me <71552968+chill-me@users.noreply.github.com>
2021-04-04 15:15:31 +0900

    20210405

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_20_200718) do
+ActiveRecord::Schema.define(version: 2021_04_04_061306) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "like_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
