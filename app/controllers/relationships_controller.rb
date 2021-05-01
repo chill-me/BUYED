1e60c20446a0e01ea7d2ea14ee4f41d484428d30
chill-me <juni428member@gmail.com>
2021-05-01 22:27:01 +0900

    20210501
    modal AJAX実装前　バックアップ用

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,8 +1,9 @@
 class RelationshipsController < ApplicationController
 
     def create
-      @user = User.find(params[:relationship][:following_id])
-      current_user.follow(@user)
+      @user = User.find(params[:relationship][:following_id])#→123などの特定の数字（user.idの部分）
+      #form_withで作られたリクエスト情報
+      @current_user.follow(@user)
       respond_to do |format|
         format.html {redirect_back(fallback_location: root_path)}
         format.js
@@ -11,7 +12,7 @@ class RelationshipsController < ApplicationController
     
     def destroy
       @user = User.find(params[:relationship][:following_id])
-      current_user.unfollow(@user)
+      @current_user.unfollow(@user)
       respond_to do |format|
           format.html {redirect_back(fallback_location: root_path)}
           format.js
