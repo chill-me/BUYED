b831a2777fbaf2f213a565e360191f7c1bd3c5f7
chill-me <juni428member@gmail.com>
2021-08-24 23:49:45 +0900

    20210824
    tag table,tag_relationship table,post table
    ruby file edited.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ApplicationRecord
 
     belongs_to :user
     has_many :likes
+    has_many :tag_relationships, dependent: :destroy
+    has_many :tags, through: :tag_relationships
 
     def liked_by?(user)
      likes.where(user_id: user.id).exists?
